f8d15574196355d4d172615d7868f3f83c12d64e
Christopher Suma <cgsuma@gmail.com>
2023-09-11 12:29:16 -0400

    before lunch monday

---
+++ DemoApi/Controllers/InfoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApi.Controllers
+{
+    public class InfoController : ControllerBase
+    {
+        [HttpGet("/info")]
+        public async Task<ActionResult> GetTheInfo()
+        {
+            return Ok($"The Controller is working just fine. created at { DateTime.Now.ToLongTimeString()}");
+        }
+    }
+}
